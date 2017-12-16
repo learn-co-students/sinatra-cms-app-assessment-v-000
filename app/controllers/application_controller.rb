bcf9d95475436460217796edb093f6408c9daa9e
Emma Feng <ff18qd@gmail.com>
2017-12-16 02:11:49 +0000

    add login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,24 @@ class ApplicationController < Sinatra::Base
       end
     end
 
+    post "/login" do
+      user = User.find_by(username: params["username"])
+      if user && user.authenticate(params["password"])
+        session[:user_id] = user.id
+        redirect :"/shoes/#{current_user.slug}"
+      else
+        redirect :"/login"
+      end
+    end
+
+    get "/logout" do
+      if logged_in?
+        session.clear
+        redirect :"/login"
+      else
+        redirect :"/"
+      end
+    end
 
     helpers do
       def logged_in?
