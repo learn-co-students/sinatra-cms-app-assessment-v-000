eb210780d413e373776adfa8522bc24a2c4c4a35
Emma Feng <ff18qd@gmail.com>
2017-12-16 15:46:00 +0000

    add slug method to Shoe class

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,12 @@
 class Shoe < ActiveRecord::Base
   belongs_to :user
 
-end 
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slugname)
+    self.all.find {|item| item.slug == slugname}
+  end
+
+end
