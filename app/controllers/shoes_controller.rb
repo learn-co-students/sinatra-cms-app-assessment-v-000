4c2e8726cbdb371ffacbcb4bc4a764c0c043d906
Emma Feng <ff18qd@gmail.com>
2017-12-16 21:24:20 +0000

    add edit to shoes

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -34,4 +34,28 @@ class ShoesController < ApplicationController
     end
   end
 
+  get "/shoes/:slug/edit" do
+    if logged_in?
+      @shoe = Shoe.find_by_slug(params[:slug])
+      if current_user.shoes.include?(@shoe)
+        erb :"shoes/edit"
+      else
+        redirect :"/users/#{current_user.slug}"
+      end
+    else
+      redirect :"/login"
+    end
+  end
+
+  patch "/shoes/:slug" do
+    if params[:name] != "" && params[:name] != nil
+      @shoe = Shoe.find_by_slug(params[:slug])
+      @shoe.update(name: params[:name], color: params[:color], brand: params[:brand])
+      redirect :"/users/#{current_user.slug}"
+    else
+      redirect :"/shoes/#{@shoe.slug}/edit"
+    end
+  end
+
+
 end
