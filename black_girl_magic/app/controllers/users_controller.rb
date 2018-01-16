7beaff5592347f4c19f85a15adb9eb162675c0d2
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-16 21:46:08 +0000

    Done.

--- black_girl_magic/app/controllers/users_controller.rb
+++ black_girl_magic/app/controllers/users_controller.rb
@@ -1,3 +1,60 @@
 class UsersController < ApplicationController
 
+  get '/users/:id' do
+    if !logged_in?
+      redirect '/bags'
+    end
+    @user = User.find(params[:id])
+    if !@user.nil? && @user == current_user
+      erb :'users/show'
+    else
+      redirect to '/events'
+    end
+  end
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to '/cities'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/events'
+    end
+  end
+
+  get '/login' do
+    @error_message = params[:error]
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect '/events'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/events"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect '/'
+    end
+  end
 end
