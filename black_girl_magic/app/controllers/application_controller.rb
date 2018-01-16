15751b65cc398542e257d39cb0a373b541f00825
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-16 16:38:28 +0000

    Done.

--- black_girl_magic/app/controllers/application_controller.rb
+++ black_girl_magic/app/controllers/application_controller.rb
@@ -5,10 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "blackgirlevents"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
---
+++ black_girl_magic/app/controllers/bgevents_controller.rb
@@ -0,0 +1,3 @@
+class BGEventsController < ApplicationController
+
+end
