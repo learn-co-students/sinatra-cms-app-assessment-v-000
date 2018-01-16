2cb8b6ef1426056a23cb5c4fc7e49f813f508fed
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-01-16 06:38:40 +0000

    added controller objects

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :'application/root'
+  end
+end
---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,10 @@
+class VenuesController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+end
