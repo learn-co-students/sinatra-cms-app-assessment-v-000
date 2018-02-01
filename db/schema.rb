6f3995d7523b7bc71a3dada3876ff0aed74e3a9a
Brittany Morris <brittmmendez@gmail.com>
2018-02-01 22:38:24 +0000

    created tables

---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180201222312) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string  "name"
+    t.string  "duration"
+    t.string  "difficulty"
+    t.string  "body_focus"
+    t.string  "training_type"
+    t.string  "equipment_needed"
+    t.string  "notes"
+    t.integer "user_id"
+  end
+
+end
