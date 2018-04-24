9c4bf3fce00ca9777116a1854c38d5cf4e3954ba
Bertrand <bertrand.chevalier@gmail.com>
2018-04-23 22:38:20 -0400

    Done.

--- cryptotransactions/app/controllers/transactions_controller.rb
+++ cryptotransactions/app/controllers/transactions_controller.rb
@@ -12,6 +12,17 @@ class TransactionsController < ApplicationController
         end
     end
 
+    post '/transactions' do
+        if params[:currency]=="" || params[:price]==""
+            redirect '/transactions/new'
+        else
+            @transaction = Transaction.create(params)
+            @transaction.user = current_user
+            @transaction.save
+            redirect "/transactions/#{@transaction.id}"
+        end
+    end
+
     get '/transactions/:id' do
         if logged_in?
             @transaction = Transaction.find(params[:id])
@@ -34,26 +45,14 @@ class TransactionsController < ApplicationController
         end
     end
 
-    post '/transactions' do
-        if params[:currency]=="" || params[:price]==""
-            @transaction = Transaction.create(params)
-            @transaction.user = current_user
-            @transaction.save
-            redirect "/transactions/#{@transaction.id}"
-        else
-            redirect '/transactions/new'
-        end
-    end
-
     patch '/transactions/:id' do
-        binding.pry
         if logged_in?
             if params[:currency]=="" || params[:price]==""
                 redirect "/transactions/#{@transaction.id}/edit"
             else
                 @transaction = Transaction.find(params[:id])
                 if @transaction && @transaction.user == current_user
-                    if @transaction.update(params)
+                    if @transaction.update(currency: params[:currency], price: params[:price], comments: params[:comments])
                         redirect "/transactions/#{@transaction.id}"
                     else
                         redirect "/transactions/#{@transaction.id}/edit"
@@ -68,5 +67,16 @@ class TransactionsController < ApplicationController
     end
 
 
+    delete '/transactions/:id/delete' do
+        if logged_in?
+          @transaction = Transaction.find(params[:id])
+          if @transaction && @transaction.user == current_user
+            @transaction.delete
+          end
+          redirect to "/users/#{current_user.slug}"
+        else
+          redirect to '/login'
+        end
+      end
 
 end
--- cryptotransactions/config.ru
+++ cryptotransactions/config.ru
@@ -5,6 +5,7 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 
+use Rack::MethodOverride
 use UsersController
 use TransactionsController
 run ApplicationController
