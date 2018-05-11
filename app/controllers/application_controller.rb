e02a826bfe3853b044469710ebe77e3e915ef33f
Elle Faraday <faraday.elle@gmail.com>
2018-05-11 17:01:02 +0000

    added no sign ups with blank fields, need unique email, error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require './config/environment'
 require 'rack-flash'
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -21,9 +23,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sign-up' do
-    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+    if params[:email] != "" && params[:name] != "" && params[:password] != "" && User.find_by_email(params[:email]) == nil
       @user = User.create(params)
       session[:user_id] = @user.id
+      redirect to '/users/:id'
+    elsif params[:email] == "" || params[:name] == "" || params[:password] == ""
+      flash[:message] = "You must fill in all required fields."
+      redirect to '/sign-up'
+    elsif User.find_by_email(params[:email]) != nil
+      flash[:message] = "An account already exists for the email you entered."
+      redirect to '/login'
     end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
