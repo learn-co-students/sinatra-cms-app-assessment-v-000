c80af118704f7cd498dc110ac932f7f4f780c92d
Elle Faraday <faraday.elle@gmail.com>
2018-05-11 14:59:45 +0000

    app files structure added

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-#require './app'
-
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
+
+require_all 'app'
