12a18f5cfc5a01821a093eac7a7d147e845b2941
Elle Faraday <faraday.elle@gmail.com>
2018-05-11 15:38:27 +0000

    set up application controller, initial route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 require './config/environment'
 require 'rack-flash'
 class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :'index'
+  end
 end
