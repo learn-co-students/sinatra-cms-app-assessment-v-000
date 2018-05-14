048fc3cfa571fa4d1b68fdbb8170099add28eaea
Elle Faraday <faraday.elle@gmail.com>
2018-05-14 16:17:24 +0000

    can delete sleeplogs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/sleeplogs' do
+    @user = User.find_by_id(session[:user_id])
+    @logs = @user.sleeplogs
+    erb :'sleeplogs/index'
+  end
+
   get '/sleeplogs/new' do
     if Helper.is_logged_in?(session)
       erb :'/sleeplogs/new'
@@ -18,22 +24,25 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/sleeplogs' do
-    @user = User.find_by_id(session[:user_id])
-    @logs = @user.sleeplogs
-    erb :'sleeplogs/index'
+  get '/sleeplogs/:id/edit' do
+    erb :'sleeplogs/edit'
   end
 
-  post '/sleeplogs' do
+  post '/sleeplogs/new' do
     if params[:hours] != "" && params[:kind] != nil && params[:date] != ""
       @log = Sleeplog.create(params)
       @user = User.find_by_id(session[:user_id])
       @user.sleeplogs << @log
-      #redirect to "/users/#{session[:user_id]}"
+      redirect to "/sleeplogs"
     else
       flash[:message] = "You must fill in all fields."
       redirect to '/sleeplogs/new'
     end
+  end
+
+  get '/sleeplogs/:id/delete' do
+    Sleeplog.find_by_id(params[:id]).delete
+    redirect to '/sleeplogs'
   end
 
 end
