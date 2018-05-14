6a6edf5d682d5a1dea29c2e818d34fccc09af87a
Elle Faraday <faraday.elle@gmail.com>
2018-05-14 15:15:39 +0000

    must be logged in to add new sleep log

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,19 @@ class UsersController < ApplicationController
   end
 
   get '/sleeplogs/new' do
-    
-    erb :'/sleeplogs/new'
+    if Helper.is_logged_in?(session)
+      erb :'/sleeplogs/new'
+    else
+      flash[:message] = "You must be logged in to add a log."
+      redirect to '/'
+    end
   end
 
   post '/sleeplogs/index' do
     @log = Sleeplog.create(params)
     @user = User.find_by_id(session[:user_id])
     @user.sleeplogs << @log
+    binding.pry
     redirect to '/'
   end
 
