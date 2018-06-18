29019141af78a7c0ce9080e1ddaef765384efdea
Michael Pope <popemray1986@gmail.com>
2018-06-18 23:34:37 +0000

    Done.

--- game_changer/app/controllers/user_controller.rb
+++ game_changer/app/controllers/user_controller.rb
@@ -5,6 +5,21 @@ class UserController < ApplicationController
     erb :'/users/show'
   end
 
+  get '/login' do
+    if logged_in?
+      erb :'/users/login'
+    else
+      redirect '/games'
+    end
+  end
 
-
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/games'
+    else
+      redirect '/signup'
+    end
+  end
 end
