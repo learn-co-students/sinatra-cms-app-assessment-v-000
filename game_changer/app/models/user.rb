1957db190a95721783c861fb14aac50f12243f3f
Michael Pope <popemray1986@gmail.com>
2018-06-18 22:12:28 +0000

    Done.

--- game_changer/app/models/user.rb
+++ game_changer/app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
   has_many :games
   has_secure_password
 
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|usrnm| usrnm.slug == slug}
+  end
+
 
 end
