54bf735b8423b41d61c9129c32ad5af9508c23e3
Michael Pope <popemray1986@gmail.com>
2018-06-18 23:24:48 +0000

    Done.

--- game_changer/app/controllers/game_controller.rb
+++ game_changer/app/controllers/game_controller.rb
@@ -35,4 +35,41 @@ class GamesController < ApplicationController
     end
   end
 
+  get '/games/:id/edit' do
+    if logged_in?
+      @game = Game.find_by_id(params[:id])
+       if @game.user_id == current_user.id
+         erb :'/games/edit'
+       else
+         redirect '/games'
+       end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/games/:id' do
+    if parmas[:title] == ""
+      redirect "/games/#{params[:id]}/edit"
+    else
+      @game = Game.find_by_id(params[:id])
+      @game.title = params[:title]
+      @game.save
+      redirect "/games/#{@game.id}"
+    end
+  end
+
+  delete '/games/:id/delete' do
+    if logged_in?
+      @game = Game.find_by_id(params[:id])
+      if @game.user_id == current_user.id
+        @game.delete
+        redirect '/games'
+      else
+        redirect '/games'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
