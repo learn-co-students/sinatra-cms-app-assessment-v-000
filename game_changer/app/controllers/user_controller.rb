eed5ab1693d111fb229d45984d7dbbb702f91e79
Michael Pope <popemray1986@gmail.com>
2018-06-19 00:09:09 +0000

    Done.

--- game_changer/app/controllers/user_controller.rb
+++ game_changer/app/controllers/user_controller.rb
@@ -22,4 +22,32 @@ class UserController < ApplicationController
       redirect '/signup'
     end
   end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/create_user'
+    else
+      redirect '/games'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/signup"
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/games"
+    end
+  end
+
+  get "/logout" do
+    if logged_in?
+      session.destroy
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
 end
