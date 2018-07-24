18baf88e15666ade0510410bb504c1667623af7d
Veronica Bustabad <vbustabad@gmail.com>
2018-07-24 14:45:07 +0000

    Done.

---
+++ db/migrate/recommendations_table.rb
@@ -0,0 +1,9 @@
+class CreateRecommendations < ActiveRecord::Migration
+
+  def change
+   create_table :recommendations do |t|
+     t.string :content
+     t.integer :user_id
+  end
+
+end
---
+++ db/migrate/requests_table.rb
@@ -0,0 +1,9 @@
+class CreateRequests < ActiveRecord::Migration
+
+  def change
+   create_table :requests do |t|
+     t.string :content
+     t.integer :user_id
+  end
+
+end
