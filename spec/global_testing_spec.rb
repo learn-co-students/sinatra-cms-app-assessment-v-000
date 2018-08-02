2ddba342ffd4b273f73c49bf61f39bb8f994c39b
Frank McCallion <fkmccallion@gmail.com>
2018-08-02 12:36:27 +0000

    Add initial spec test and spec helper from Fwitter repository

--- spec/global_testing_spec.rb
+++ spec/global_testing_spec.rb
@@ -3,4 +3,12 @@ require 'pry'
 
 describe GlobalTesting do
 
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to DBCA")
+    end
+  end
+
 end
