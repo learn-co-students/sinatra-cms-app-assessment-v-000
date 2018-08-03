afcfbca805a7ca25d1f00f59a9fa8cca473275e1
Frank McCallion <fkmccallion@gmail.com>
2018-08-03 13:53:45 +0000

    update model creation spec test

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ActiveRecord::Base
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :location
+  belongs_to :tag
 
   def slug
     name.downcase.gsub(" ","-")
--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -8,9 +8,11 @@ describe 'Models and Tables' do
     @category = Category.create(:name => "Directory")
     @county = County.create(:name => "Ventura")
     @location = Location.create(:county_id => @county.id, :city => "Somis", :zip => 93066, :locality => "Hwy 118")
-    @org = Organization.create(:user_id => @user.id, :category_id => @category.id, :location_id => @location.id, :name => "DBCA", :address => "1111 Big Sur Rd.", :phone => "805-867-5309", :email => "org@test.com", :website => "www.dbca.com", :account => "Colonel", :description => "CRUD Application Interface")
-    @rank = Rank.create(:aggregate_value => 1, :daily_value => 1, :sort => 1)
-    @tag = Tag.create(:name)
+    @tag1 = Tag.create(:name => "Hobbies")
+    @tag2 = Tag.create(:name => "Local Clubs")
+    @tag3 = Tag.create(:name => "Government Agencies")
+    @tag4 = Tag.create(:name => "Contractors")
+    @org = Organization.create(:user_id => @user.id, :category_id => @category.id, :location_id => @location.id, :name => "DBCA", :address => "1111 Big Sur Rd.", :phone => "805-867-5309", :email => "org@test.com", :website => "www.dbca.com", :account => "Colonel", :description => "CRUD Application Interface", :aggregate_rank => 1, :daily_rank => 1, :tag1 => @tag1.id, :tag2 => @tag2.id, :tag3 => @tag3.id, :tag4 => @tag4.id)
   end
   it 'can slug the org name' do
     expect(@organization.slug).to eq("dbca")
