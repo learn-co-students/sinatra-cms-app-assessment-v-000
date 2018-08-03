375a3f6328a5daa7ce81314cda038a9162e7343f
Frank McCallion <fkmccallion@gmail.com>
2018-08-03 12:17:45 +0000

    update spec test for user and create organization

--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -4,7 +4,9 @@ require 'pry'
 describe 'User' do
 
   before do
-    @user = User.create(:nickname => "DBCA creator")
+    @user = User.create(:first_name => "Frank", :last_name => "Mccallion", :nickname => "Keith", :title => "DBCA Creator", :email => "user@test.com", :password => "password")
+    @organization = Organization.create(:name => "DBCA", :address => "1111 Big Sur Rd." :phone => "805-867-5309", :email => "org@test.com", :website => "www.dbca.com", :user_id => @user.id)
+
   end
   it 'can slug the nickname' do
     expect(@user.slug).to eq("dbca-creator")
