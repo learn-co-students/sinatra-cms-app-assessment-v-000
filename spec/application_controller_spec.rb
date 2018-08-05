20f5a0ad78ca47bf2d8d90d3ac6959b1142625bb
Frank McCallion <fkmccallion@gmail.com>
2018-08-05 18:34:15 +0000

    update test spec to include signup reqs

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -4,19 +4,17 @@ require 'pry'
 describe ApplicationController do
 
   describe "Homepage" do
-
     it 'loads the homepage' do
       get '/'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to include("Welcome to DBCA Admin Panel!")
     end
-
   end
 
   describe "Signup Page" do
 
     it "Loads the signup page" do
-      get '/signup' do
+      get '/signup'
       expect(last_response.status).to eq(200)
     end
 
@@ -96,6 +94,20 @@ describe ApplicationController do
       }
       post '/signup', params
       expect(last_response.location).to include('/signup')
+    end
+
+    it 'creates a new user and logs them in on valid submission and does not let a logged in user view the signup page' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "DCBA Developer",
+        :password => "password"
+      }
+      post '/signup', params
+      get '/signup'
+      expect (last_response.location).to include('/user')
     end
 
   end
