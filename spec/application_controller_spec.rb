8b92c756e9e801076b4e62a133476804faa1c76f
Frank McCallion <fkmccallion@gmail.com>
2018-08-05 18:27:51 +0000

    update application controller spec test

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -13,11 +13,89 @@ describe ApplicationController do
 
   end
 
-  describe "Developer Page" do
-    #Add login feature later on
-    it 'loads the developer page' do
-      get '/developer'
+  describe "Signup Page" do
+
+    it "Loads the signup page" do
+      get '/signup' do
       expect(last_response.status).to eq(200)
+    end
+
+    it 'signup directs user to user index' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "DCBA Developer",
+        :password => "password"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/user")
+    end
+
+    it 'does not let a user sign up without a first_name' do
+      params = {
+        :first_name => "",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "DCBA Developer",
+        :password => "password"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a last_name' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "DCBA Developer",
+        :password => "password"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without an email' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "",
+        :title => "DCBA Developer",
+        :password => "password"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a title' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "",
+        :password => "password"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a password' do
+      params = {
+        :first_name => "Frank",
+        :last_name => "Mccallion",
+        :nickname => "Keith",
+        :email => "keith@test.com",
+        :title => "DCBA Developer",
+        :password => ""
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
     end
 
   end
