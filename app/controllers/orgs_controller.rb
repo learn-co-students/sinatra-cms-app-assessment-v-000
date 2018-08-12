edb83ff9892d8e172101262ebc2617e4f0999bb6
Frank McCallion <fkmccallion@gmail.com>
2018-08-12 23:25:32 +0000

    update editing functions

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -22,6 +22,20 @@ class OrgsController < ApplicationController
     erb :"orgs/edit"
   end
 
+  get '/orgs/:id/delete' do
+    if logged_in?
+      @org = Organization.find(params[:id])
+      if @org && @org.user == current_user
+        @org.delete
+      elsif @org && current_user.email == "fkmccallion@hotmail.com"
+        @org.delete
+      end
+      redirect '/account'
+    else
+      redirect '/login'
+    end
+  end
+
   post '/orgs/new' do
     @org = Organization.create(params)
     @org.user_id = current_user.id
@@ -32,14 +46,27 @@ class OrgsController < ApplicationController
   post '/orgs/:id/edit' do
     @org = Organization.find(params[:id])
     if @org && @org.user == current_user
-      params.each do |k,v|
-        if !(v == "")
-          binding.pry
-          #@org.k = v
+      params.each_pair do |k,v|
+        if !(v.empty? || k == "id")
+          case k
+          when "name"
+            @org.name = v
+          when "address"
+            @org.address = v
+          when "phone"
+            @org.phone = v
+          when "email"
+            @org.email = v
+          when "website"
+            @org.website = v
+          when "description"
+            @org.description = v
+          end
         end
       end
     end
-    binding.pry
+    @org.save
+
     redirect '/account'
   end
 
