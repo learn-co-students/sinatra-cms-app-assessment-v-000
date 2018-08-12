67efbc02085236cfa74c233cc3553986cac15769
Frank McCallion <fkmccallion@gmail.com>
2018-08-12 18:52:45 +0000

    add edit and show org views

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -13,13 +13,33 @@ class OrgsController < ApplicationController
   end
 
   get '/orgs/:id/show' do
+    @org = Organization.find(params[:id])
+    erb :"orgs/show"
+  end
 
+  get '/orgs/:id/edit' do
+    @org = Organization.find(params[:id])
+    erb :"orgs/edit"
   end
 
   post '/orgs/new' do
     @org = Organization.create(params)
     @org.user_id = current_user.id
     @org.save
+    redirect '/account'
+  end
+
+  post '/orgs/:id/edit' do
+    @org = Organization.find(params[:id])
+    if @org && @org.user == current_user
+      params.each do |k,v|
+        if !(v == "")
+          binding.pry
+          #@org.k = v
+        end
+      end
+    end
+    binding.pry
     redirect '/account'
   end
 
