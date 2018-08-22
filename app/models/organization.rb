e274391d157734d7c2b17aec56949696ddbeb15e
Frank McCallion <fkmccallion@gmail.com>
2018-08-22 12:50:56 +0000

    add sorting logic - resetting values to come later

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,20 @@ class Organization < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  def account_rank
+    case self.account
+    when "General"
+      6
+    when "Colonel"
+      5
+    when "Major"
+      4
+    when "Captain"
+      3
+    when "Lieutenant"
+      2
+    end
+  end
 
   def slug
     name.downcase.gsub(" ","-")
